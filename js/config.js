05956a06bb5173a7fd64fa168d26d9870d9c3abf
bilalbezar <33976214+bilalbezar@users.noreply.github.com>
2024-12-20 11:08:31 +0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_UpqsHnQ6l', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '20jp7mv1edgngq7f8devjl3kh3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'o2omoi6gb42u9cqq8qjvtmln4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
